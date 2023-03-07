aa6476130d00fc4abce10afaf20786c9bce6f0b7
Sandesh040602 <76606989+Sandesh040602@users.noreply.github.com>
2023-03-07 15:32:01 +0530

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,24 @@
+int parent[200005], size[200005];
+// make sure to submit it under the c++14
+// else change the name of the size array
+void make_set(int v) {
+    parent[v] = v;
+    size[v] = 1;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}
