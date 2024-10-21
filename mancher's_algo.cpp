55d0ca43d02e0b3783a3af72d163dd5c5fec40eb
Sandesh040602 <76606989+Sandesh040602@users.noreply.github.com>
2024-10-21 06:54:46 +0530

    Update mancher's_algo.cpp

--- mancher's_algo.cpp
+++ mancher's_algo.cpp
@@ -1,15 +1,22 @@
 vector<int> manachers(string s){
-    string s1 = ""; s1+="#";
-//     dbg(s1.length());
-    for(int i=0;i<s.length();i++){  s1+=s[i];  s1+="#";  }
-//     dbg(s1)
+    string s1 = ""; 
+    s1+="#";
+    
+    for(int i=0;i<s.length();i++){
+        s1+=s[i];
+        s1+="#";
+    }
+    
     vector<int>manc(s1.length(), 1ll);
     int c = 0, r=0;
     for(int i=1;i<s1.length();i++){
-        int m_id = c - (i-c); 
+        int m_id = c - (i-c); // mirror index
         if(i<r){manc[i] = min(r-i, manc[m_id]);}
         while(i+manc[i]<s1.length() && i-manc[i]>=0 &&s1[i+manc[i]]==s1[i - manc[i]]){manc[i]++;}
-        if(i+manc[i]>r){ r = i+manc[i]; c = i; }
+        if((i+manc[i])>r){
+            r = i+manc[i];
+            c = i;
+        }
     }
     return manc;
 }
@@ -20,7 +27,7 @@ bool haspm(int l, int r, vector<int>&manc){
     int a1 = 2*l +1, a2 = 2*r +1;
     // now calculate the center
     int mid = a1+(a2-a1)/2;
-    dbg(mid)
+
     if(mid%2)return manc[mid]>=(r-l+1);
     if(mid%2==0)return manc[mid]>=(r-l+2);
 }
